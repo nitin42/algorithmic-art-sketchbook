7224febbeec9bc721f49318eb9b91117b03e4bcd
Nitin <tulswani19@gmail.com>
2018-11-22 17:00:01 +0530

    revamp home page

---
+++ src/pages/Footer.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+export default function Footer(props) {
+  return (
+    <p className="link">
+    by{' '}
+      <a
+        href="https://nitin-tulswani.surge.sh/"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Nitin Tulswani
+      </a>
+    </p>
+  )
+}
--- src/pages/Introduction.js
+++ src/pages/Introduction.js
@@ -1,17 +1,19 @@
 import React from 'react'
 
 import Intro from './content/Introduction.mdx'
+import Footer from './Footer'
 
 export default function Introduction(props) {
   return (
     <div className="center" style={{ flexDirection: 'column'}}>
       <h1 className="heading">Shader World</h1>
-      <div className="intro" id="content">
+      <div className="intro" id="content" style={{ marginTop: -20 }}>
         <Intro />
         <div className="center">
         <a href="" id="explore-link">Explore &rarr;</a>
         </div>
       </div>
+      <Footer />
     </div>
   )
 }
